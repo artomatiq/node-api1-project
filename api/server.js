5ebe70317d15e9ed8526ae13961177d47192178e
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-02 09:48:04 -0400

    get with id

--- api/server.js
+++ api/server.js
@@ -30,15 +30,31 @@ server.post('/api/users', async (req, res) => {
         }
         else {
             const newUser = await User.insert({name, bio})
-            res.status(201).json({
-                data: newUser
-            })
+            res.status(201).json(newUser)
         }
     }
     catch {
         res.status(500).json({message: 'There was an error while saving the user to the database'})
     }
 })
+
+//get specific user
+server.get(`/api/users/:id`, (req, res) => {
+    const {id} = req.params
+    User.findById(id)
+        .then (user => {
+            if (!user) {
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+            }
+            else {
+                res.status(201).json(user)
+            }
+        })
+        .catch (
+            res.status(500).json({message: 'The user information could not be retrieved',})
+        )
+})
+
 
 
 
