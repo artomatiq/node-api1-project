85e6406b74a003121bc7171c46694b05e0dc088b
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-02 09:22:05 -0400

    get and post requests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,45 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
+const server = express()
+server.use(express.json())
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+//get all users
+server.get('/api/users', (req, res)=> {
+    User.find()
+        .then (users => {
+            console.log(users)
+            res.status(200).json(users)
+        })
+        .catch (err => {
+            res.status(500).json({
+                message: 'The users information could not be retrieved',
+                error: err
+            })
+        })
+})
+
+//add a new user
+server.post('/api/users', async (req, res) => {
+    const {name, bio} = req.body;
+    console.log('this is the request body', req.body)
+    try {
+        if (!name || !bio) {
+            res.status(400).json({message: 'Please provide name and bio for the user'})
+        }
+        else {
+            const newUser = await User.insert({name, bio})
+            res.status(201).json({
+                data: newUser
+            })
+        }
+    }
+    catch {
+        res.status(500).json({message: 'There was an error while saving the user to the database'})
+    }
+})
+
+
+
+module.exports = server; 
